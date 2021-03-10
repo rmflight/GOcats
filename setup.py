fc7b367583a5a579a76c58a83a37fe13c69ebccc
rmflight <r.flight79@gmail.com>
2021-03-10 16:26:58 -0500

    making sure to ignore docs folder too

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['doc', 'vignettes']),
+    packages=find_packages(exclude=['doc', 'docs', 'vignettes']),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
